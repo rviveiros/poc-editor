eaffa09d101141997d81d5cbd574f36cfe2838a1
Rafael Viveiros <eu@rafaelv.com.br>
2014-03-07 13:48:19 -0500

    high inicio

--- ace-noconflict/mode-care.js
+++ ace-noconflict/mode-care.js
@@ -1,4 +1,4 @@
-define(function(require, exports, module) {
+ace.define('ace/mode/care', function(require, exports, module) {
 "use strict";
 
 var oop = require("../lib/oop");
@@ -8,8 +8,8 @@ var Tokenizer = require("../tokenizer").Tokenizer;
 var MatchingBraceOutdent = require("./matching_brace_outdent").MatchingBraceOutdent;
 
 // defines the language specific highlighters and folding rules
-var MyNewHighlightRules = require("./mynew_highlight_rules").MyNewHighlightRules;
-var MyNewFoldMode = require("./folding/mynew").MyNewFoldMode;
+var MyNewHighlightRules = require("./highlight_rules").MyNewHighlightRules;
+var MyNewFoldMode = require("./folding").MyNewFoldMode;
 
 var Mode = function() {
     // set everything up
@@ -53,3 +53,86 @@ oop.inherits(Mode, TextMode);
 
 exports.Mode = Mode;
 });
+
+ace.define('ace/mode/care/highlight_rules', function(require, exports, module) {
+"use strict";
+
+var oop = require("../lib/oop");
+var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
+
+
+var colors = lang.arrayToMap(
+    ("aqua|black|blue|fuchsia|gray|green|lime|maroon|navy|olive|orange|" +
+    "purple|red|silver|teal|white|yellow").split("|")
+);
+
+var fonts = lang.arrayToMap(
+    ("arial|century|comic|courier|garamond|georgia|helvetica|impact|lucida|" +
+    "symbol|system|tahoma|times|trebuchet|utopia|verdana|webdings|sans-serif|" +
+    "serif|monospace").split("|")
+);
+
+
+var MyNewHighlightRules = function() {
+
+    // regexp must not have capturing parentheses. Use (?:) instead.
+    // regexps are ordered -> the first match is used
+   this.$rules = {
+        "start" : [
+            {
+                token: "lista1", // String, Array, or Function: the CSS token to apply
+                regex: /\b(eu|vc|ele|nos)\b/ //, // String or RegExp: the regexp to match
+                //next:  <next>   // [Optional] String: next state to enter
+            },
+            {
+                token : "constant.language.boolean",
+                regex : /(?:true|false)\b/
+            },
+            {
+                token: function(value) {
+                    if (colors.hasOwnProperty(value.toLowerCase())) {
+                        return "support.constant.color";
+                    }
+                    else if (fonts.hasOwnProperty(value.toLowerCase())) {
+                        return "support.constant.fonts";
+                    }
+                    else {
+                        return "text";
+                    }
+                },
+                regex: "\\-?[a-zA-Z_][a-zA-Z0-9_\\-]*"
+            }
+        ]
+    };
+};
+
+oop.inherits(MyNewHighlightRules, TextHighlightRules);
+
+exports.MyNewHighlightRules = MyNewHighlightRules;
+
+});
+
+ace.define('ace/mode/care/folding', define(function(require, exports, module) {
+"use strict";
+
+var oop = require("../../lib/oop");
+var Range = require("../../range").Range;
+var BaseFoldMode = require("./fold_mode").FoldMode;
+
+var FoldMode = exports.FoldMode = function() {};
+oop.inherits(FoldMode, BaseFoldMode);
+
+(function() {
+
+    // regular expressions that identify starting and stopping points
+    this.foldingStartMarker; 
+    this.foldingStopMarker;
+
+    this.getFoldWidgetRange = function(session, foldStyle, row) {
+        var line = session.getLine(row);
+
+        // test each line, and return a range of segments to collapse
+    };
+
+}).call(FoldMode.prototype);
+
